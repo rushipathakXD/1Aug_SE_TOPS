91a15a645dab4423efba65b7715469919a5e296b
rushipathakXD <139609131+rushipathakXD@users.noreply.github.com>
2025-08-16 11:42:24 +0530

    Add files via upload

---
+++ Task/C Programming/DataTypes_FormatSpecifier.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	//declaring different variables with diffrent data types
+	
+	int a=10;
+	float b=7.5478;
+	char ch='R';
+	double c=18.41257856;	
+	
+	printf("\n int a = %d",a);
+	printf("\n float b= %f ",b);
+	printf("\n char ch = %c ",ch);
+	printf("\n double c= %lf ",c);
+	
+}
