460ef89f682cb9b4a0163118a7578bcaf3d4deee
rushipathakXD <139609131+rushipathakXD@users.noreply.github.com>
2025-12-25 10:44:56 +0530

    Add files via upload

---
+++ Task/Python-Basics/StringExample.py
@@ -0,0 +1,13 @@
+str1 = input("Enter a String: ")
+
+t = str1.startswith("R")
+print(t)
+
+str2 = str1.replace(" ", "*")
+print(str2)
+
+print(str1.find("Rushi"))
+
+print(str1.strip())
+print(str1.lstrip())
+print(str1.rstrip())
